ae1ec7b8cd2c949aeb4245d539bb3f0f3143840d
Kevin_Gillispie <oakenquill@gmail.com>
2017-08-25 19:52:33 -0700

    init commit

---
+++ ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Asset Tracker</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 242px;
+            height: 300px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img class="auto-style1" src="epic-spies-logo.jpg" style="height: 150px; width: auto;" /><br /></div>
+            <div><h2 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Asset Performance Tracker</h2>
+        </div>
+        <div>
+            <p>Asset Name: 
+                <asp:TextBox ID="assetNameBox" runat="server"></asp:TextBox>
+            </p>
+            <p>Number of Elections Rigged:
+                <asp:TextBox ID="electionsRiggedBox" runat="server"></asp:TextBox>
+            </p>
+            <p>Number of Acts of Subterfuge Performed:
+                <asp:TextBox ID="subterfugeBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="assetButton" runat="server" Text="Add Asset" OnClick="assetButton_Click" />
+            </p>
+            <p>
+                <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (!Page.IsPostBack)
+            {
+                string[] asset = new string[0];
+                double[] elections = new double[0];
+                double[] subterfuge = new double[0];
+
+                ViewState.Add("Asset", asset);
+                ViewState.Add("Elections", elections);
+                ViewState.Add("Subterfuge", subterfuge);
+            }
+            
+        }
+
+        protected void assetButton_Click(object sender, EventArgs e)
+        {
+
+            string[] asset = (string[])ViewState["Asset"];
+            double[] elections = (double[])ViewState["Elections"];
+            double[] subterfuge = (double[])ViewState["Subterfuge"];
+
+            Array.Resize(ref asset, asset.Length + 1);
+            Array.Resize(ref elections, elections.Length + 1);
+            Array.Resize(ref subterfuge, subterfuge.Length + 1);
+
+            int assetIndex = asset.GetUpperBound(0);
+            int elecIndex = elections.GetUpperBound(0);
+            int subtIndex = subterfuge.GetUpperBound(0);
+
+            asset[assetIndex] = assetNameBox.Text;
+            elections[elecIndex] = double.Parse(electionsRiggedBox.Text);
+            subterfuge[subtIndex] = double.Parse(subterfugeBox.Text);
+
+            ViewState["Asset"] = asset;
+            ViewState["Elections"] = elections;
+            ViewState["Subterfuge"] = subterfuge;
+
+            // Sum of elections rigged
+            // Average of subterfuge
+            // Last name entered
+
+            double sumElec = elections.Sum();
+            double aveSubt = subterfuge.Average();
+
+            resultLabel.Text = String.Format("Total Elections Rigged: {0}<br>"
+                + "Average Acts of Subterfuge per Asset: {1:N2}<br>"
+                + "(Last Asset Added: {2})",
+                sumElec,
+                aveSubt,
+                asset[assetIndex]);
+
+            // Clear the textboxes:
+            assetNameBox.Text = "";
+            electionsRiggedBox.Text = "";
+            subterfugeBox.Text = "";
+        }
+    }
+}
